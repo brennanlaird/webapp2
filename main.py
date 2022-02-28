29c4248c55580a22b5b3a3e19ab58a9324a33cc8
brennanlaird <brennan.laird@gmail.com>
2022-02-28 12:47:51 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# @ is a decorator - a way to wrap a function and modify its behavior
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
+# Changes made
