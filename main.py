a16f28e421f7392b3d22bfa3e6524999624811c1
brennanlaird <brennan.laird@gmail.com>
2022-03-01 09:49:54 -0700

    test

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import yfinance as yf
+import json
+import plotly
+import plotly.graph_objects as go
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
 app = Flask(__name__)
 stock_list = ['AAPL', 'GME', 'MSFT', 'TSLA']
+
 
 # @ is a decorator - a way to wrap a function and modify its behavior
 @app.route('/', methods=['GET', 'POST'])
@@ -26,11 +35,21 @@ def get_data():
     # Adding the 50-day and 200-day moving averages to the dataframe
     price_data['50dayMA'] = price_data.Close.rolling(50).mean()
     price_data['200dayMA'] = price_data.Close.rolling(200).mean()
-    #print(price_data.head())
-    #print(price_data.info())
-    return render_template("index.html", ticker=ticker, stock_list=stock_list)
+
+
+    price_fig = go.Figure()
+
+    price_fig.add_trace(go.Scatter(x=list(price_data.index), y=list(price_data['Close'])))
+
+    title = ticker + " Price"
+    price_fig.update_layout(title_text=title)
+
+    priceJSON = json.dumps(price_fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # print(price_data.head())
+    # print(price_data.info())
+    return render_template("index.html", ticker=ticker, stock_list=stock_list, priceJSON=priceJSON)
+
 
 if __name__ == '__main__':
     app.run()
-
-
